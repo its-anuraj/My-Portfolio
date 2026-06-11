c660a8f212db139e5e3c28aeeb068aff2c351cbd
its-anuraj <ajsinghindolia@gmail.com>
2026-06-11 20:50:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  allowedDevOrigins: ["floral-speed-gravel.ngrok-free.dev"],
+  images: {
+    qualities: [25, 50, 75, 100],
+  },
+};
 
 export default nextConfig;
